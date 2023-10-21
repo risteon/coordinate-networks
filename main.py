c0480b51fb2e461ec0fb8f14dcff4ed4d21f7a89
Christoph Rist <c.rist@posteo.de>
2023-10-21 13:30:32 +0200

    Add positional encoding

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_model():
     """Create 4 layer MLP with 3 output neurons."""
     model = nn.Sequential(
         nn.Flatten(),
-        nn.Linear(2, 128),
+        nn.Linear(32, 128),
         nn.ReLU(),
         nn.Linear(128, 64),
         nn.ReLU(),
@@ -34,30 +34,62 @@ def get_model():
     return model
 
 
-def train(model, writer, coord_inputs, coord_outputs, steps=2001):
+def add_positional_encoding(coords: torch.Tensor):
+    """Add positional sin cos encoding to [B, 2] coords."""
+    # [B, 2]
+    coords = coords.clone().to(torch.float32)
+    # [B, 2]
+    coords -= 15.5
+    # [B, 2, 1]
+    coords = coords.unsqueeze(-1)
+    # [B, 2, 8]
+    sin = torch.sin(coords / 16.0 ** (2 * torch.arange(0, 16, 2) / 8))
+    # [B, 2, 8]
+    cos = torch.cos(coords / 16.0 ** (2 * torch.arange(1, 16, 2) / 8))
+    # [B, 2, 16]
+    sin_cos = torch.cat([sin, cos], dim=-1)
+    # [B, 32]
+    sin_cos = sin_cos.flatten(start_dim=1)
+    return sin_cos
+
+
+def train(model, writer, coord_inputs, coord_outputs, coords_orig, steps=30000):
     """Train model on dataset."""
+    # count all optimizer steps
+    global_step = 0
+
+    epoch_size = 10000
     # create optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     # create loss function
     loss_fn = nn.SmoothL1Loss()
+    # create lr scheduler
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(
+        optimizer, gamma=0.5, verbose=True
+    )
     # loop over epochs
-    for step in range(steps):
-        # zero gradients
-        optimizer.zero_grad()
-        # forward pass
-        y_hat = model(coord_inputs)
-        # compute loss
-        loss = loss_fn(y_hat, coord_outputs)
-        # backward pass
-        loss.backward()
-        # update parameters
-        optimizer.step()
-        # print loss
-        print(f"Step {step} | Loss {loss.item()}")
-        
-        # call add_image every 100 steps
-        if step % 100 == 0:
-            add_image(coord_inputs, y_hat, writer, step)
+    for epoch in range(1, steps // epoch_size + 1):
+        for step in range(1, epoch_size + 1):
+            # zero gradients
+            optimizer.zero_grad()
+            # forward pass
+            y_hat = model(coord_inputs)
+            # compute loss
+            loss = loss_fn(y_hat, coord_outputs)
+            # backward pass
+            loss.backward()
+            # update parameters
+            optimizer.step()
+            global_step += 1
+            # print loss
+            if step % 100 == 0:
+                print(f"Step {global_step} | Loss {loss.item()}")
+            if step % 1000 == 0:
+                # call add_image for every epoch
+                add_image(coords_orig, y_hat, writer, global_step)
+            
+        # update lr scheduler
+        scheduler.step()
 
 
 def add_image(coords, values_rgb, writer, step):
@@ -65,13 +97,7 @@ def add_image(coords, values_rgb, writer, step):
     
     Then add to summary writer.
     """
-    coords = coords.cpu().detach()
     values_rgb = values_rgb.cpu().detach()
-    # float, move and scale
-    coords *= 15.5
-    coords += 15.5
-    # round coords to nearest integer
-    coords = torch.round(coords).to(torch.int64)
     # make image
     img = torch.zeros((32, 32, 3), dtype=torch.float32)
     img[coords[:, 0], coords[:, 1], :] = values_rgb
@@ -100,14 +126,12 @@ def main(example_index: int = 255):
     index = torch.stack([x, y], dim=-1)
     # [32*32, 2]
     index = index.reshape((-1, 2))
-    # float, move and scale
-    index = index.to(torch.float32)
-    index -= 15.5
-    index /= 15.5
+    # apply positional encoding
+    encoded = add_positional_encoding(index)
     
     outputs = img.permute(1, 2, 0).reshape((-1, 3))
     
-    train(model.cuda(), writer, index.cuda(), outputs.cuda())
+    train(model.cuda(), writer, encoded.cuda(), outputs.cuda(), index)
 
 if __name__ == "__main__":
     main()
